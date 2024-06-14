11a6efd750f05cbf046e37666ef7cbb8d7cd8184
Raj Karmakar <97rajmath@gmail.com>
2024-06-14 16:58:24 +0530

    Added NestConfig Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { UserModule } from './user/user.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UserModule, BookmarkModule, AuthModule, PrismaModule],
+  imports: [UserModule, BookmarkModule, AuthModule, PrismaModule, ConfigModule.forRoot({ isGlobal: true })],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,15 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
-	constructor () {
+	constructor (private configService: ConfigService) {
 		super({
 			datasources: {
 				db: {
-					url: 'mysql://root:P@ssw0rd@localhost:3306/nestjs?schema=public'
+					url: configService.get('DATABASE_URL')
 				}
 			}
 		})
+		// console.log(configService.get('DATABASE_URL'))
 	}
 }
