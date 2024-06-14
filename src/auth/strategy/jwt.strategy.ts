915715b96eb41734b97eb678a9378fb1aa0af712
Raj Karmakar <97rajmath@gmail.com>
2024-06-14 17:49:51 +0530

    Used AuthGuard & Strategy

---
+++ src/auth/strategy/jwt.strategy.ts
@@ -0,0 +1,15 @@
+import { Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { PassportStrategy } from "@nestjs/passport";
+import { ExtractJwt, Strategy } from "passport-jwt";
+
+@Injectable()
+export class JWTStrategy extends PassportStrategy(Strategy, 'jwt') {
+	constructor(private configService: ConfigService) {
+		super({
+			jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+			ignoreExpiration: false,
+			secretOrKey: configService.get('JWT_SECRET'),
+		});
+	}
+}
