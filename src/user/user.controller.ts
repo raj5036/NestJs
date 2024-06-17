890c5aa2e08e56bd0240a434fb6f172220e0b0e0
Raj Karmakar <97rajmath@gmail.com>
2024-06-17 10:07:38 +0530

    Create Custom Guard for JWT Auth

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,11 +1,14 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
+import { JWTGuard } from './../auth/guard/index';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+// import { AuthGuard } from '@nestjs/passport';
+import { Request } from 'express';
 
 @Controller('user')
 export class UserController {
-	@UseGuards(AuthGuard('jwt'))
+	@UseGuards(JWTGuard)
 	@Get('protected-route')
-	getHello(): string {
-		return 'Hello World!';
+	getHello(@Req() req: Request) {
+		console.log({user: req.user})
+		return req.user;
 	}
 }
