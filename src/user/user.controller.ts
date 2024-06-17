803d9c1fef21dcb1e985d1b428c84a85a199e039
Raj Karmakar <97rajmath@gmail.com>
2024-06-17 10:33:40 +0530

    Use HttpCode decorator for custom statusCode

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,5 +1,5 @@
 import { JWTGuard } from './../auth/guard/index';
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Controller, Get, HttpCode, HttpStatus, Req, UseGuards } from '@nestjs/common';
 import { User } from '@prisma/client';
 // import { AuthGuard } from '@nestjs/passport';
 import { Request } from 'express';
@@ -8,6 +8,7 @@ import { GetUser } from 'src/auth/decorator';
 @UseGuards(JWTGuard)
 @Controller('user')
 export class UserController {
+	@HttpCode(HttpStatus.OK)
 	@Get('protected-route')
 	getHello(@Req() req: Request, @GetUser() user: User, @GetUser('email') email: string) {
 		console.log({user: req.user})
